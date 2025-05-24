322bb7a5b9e19ba3d7f89338c5b4b1be71d79967
andrew0samir <andrewsamir317@gmail.com>
2025-05-24 17:52:51 +0300

    created local server to fake api , added script to watch questions data , used hooks to fetch data and handle state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+    questions: [],
+    // 'loading' , 'error' , 'ready' , 'active' , finished'
+    status: 'loading',
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataRecevied':
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready"
+            };
+        case 'dataFailed':
+            return {
+                ...state,
+                status: "error"
+            }
+        default:
+            throw new Error("Unknown Action");
+    }
+}
+
+
 export default function App() {
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    useEffect(function () {
+        fetch('http://localhost:9000/questions')
+            .then(res => res.json())
+            .then(data => dispatch({ type: "dataRecevied", payload: data }))
+            .catch(err => dispatch({ type: "dataFailed" }))
+    }, [])
+
     return <div className="app">
         <Header />
 
@@ -9,6 +45,6 @@ export default function App() {
             <p>1/15</p>
             <p>Question?</p>
         </Main>
-        
+
     </div>
 }
