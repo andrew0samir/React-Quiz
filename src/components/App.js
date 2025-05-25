688de122b71d6174578499dad863ded8f4f008dc
andrew0samir <andrewsamir317@gmail.com>
2025-05-25 22:26:49 +0300

    added functionality to finish and restart , handled score and result page

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from './StartScreen';
 import Question from './Question';
 import NextButton from './NextButton';
 import Progress from './Progress';
+import FinishScreen from './FinishScreen';
 
 const initialState = {
     questions: [],
@@ -15,6 +16,7 @@ const initialState = {
     index: 0,
     answer: null,
     points: 0,
+    highscore: 0,
 };
 
 function reducer(state, action) {
@@ -50,6 +52,19 @@ function reducer(state, action) {
                 index: state.index + 1,
                 answer: null,
             };
+        case 'finish':
+            return {
+                ...state,
+                status: "finished",
+                highscore: state.points > state.highscore ?
+                    state.points : state.highscore
+            };
+        case 'restart':
+            return {
+                ...initialState,
+                questions: state.questions,
+                status: 'ready',
+            }
         default:
             throw new Error("Unknown Action");
     }
@@ -58,7 +73,7 @@ function reducer(state, action) {
 
 export default function App() {
 
-    const [{ questions, status, index, answer, points }, dispatch] = useReducer(reducer, initialState)
+    const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState)
 
     const numQuestions = questions.length;
     const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
@@ -98,8 +113,22 @@ export default function App() {
                         dispatch={dispatch}
                         answer={answer}
                     />
-                    <NextButton dispatch={dispatch} answer={answer} />
+                    <NextButton
+                        dispatch={dispatch}
+                        answer={answer}
+                        index={index}
+                        numQuestions={numQuestions}
+                    />
                 </>
+            }
+            {
+                status === "finished" &&
+                <FinishScreen
+                    points={points}
+                    maxPossiblePoints={maxPossiblePoints}
+                    highscore={highscore}
+                    dispatch={dispatch}
+                />
             }
         </Main>
 
